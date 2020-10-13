71a0d61be4d52d4e72fb5ae6bfa4b5e50dc86104
wgy <32060343+wuguanyang@users.noreply.github.com>
2020-10-13 12:00:29 +0800

    init demo

---
+++ Assets/GetIdentertyControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;//Must be Defined
+
+public class GetIdentertyControl : MonoBehaviour {
+
+    // Use this for initialization
+    public Text IDFA;
+    public Text Verdorid;
+
+    [DllImport("__Internal")]
+    private static extern void _GetIDFA();
+
+    public void GetIDFA(string str) {
+        if (string.IsNullOrEmpty(str)) {
+            Debug.Log("IDFA is NULL");
+            return;
+        }
+        IDFA.text = str;
+    }
+
+    [DllImport("__Internal")]
+    private static extern void _GetVerdorid();
+
+    public void GetVerdorid(string str) {
+        if (string.IsNullOrEmpty(str)) {
+            Debug.Log("Verdorid is NULL");
+            return;
+        }
+        Verdorid.text = str;
+    }
+    public void OnClickButton() {
+        if (Application.platform != RuntimePlatform.OSXEditor) {
+            _GetVerdorid();
+            _GetIDFA();
+        }
+    }
+}
